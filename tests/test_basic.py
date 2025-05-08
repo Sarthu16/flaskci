0f38c5dbe7afda183678ab50c98ec6270a6860f2
Sarthak Kulkarni <sarthakkulkarni1507@gmail.com>
2025-05-08 11:58:45 +0530

    test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,2 +1,13 @@
-def test_example():
-    assert 1 + 1 == 2
+import pytest
+from app import create_app
+
+@pytest.fixture
+def client():
+    app = create_app()
+    app.config['TESTING'] = True
+    return app.test_client()
+
+def test_home_route(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Hello" in response.data
