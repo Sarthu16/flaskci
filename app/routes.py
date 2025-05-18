be2008a745bc076d6937c42a694dee17f8513565
Sarthu16 <sarthakkulkarni1507@gmail.com>
2025-05-18 15:38:54 +0530

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,4 @@ main = Blueprint("main", __name__)
 
 @main.route("/")
 def home():
-    return "Hello, Flask CI/CD! web app made cheap and fast with Flask!!!!! I made this using jenkins"
+    return "Hello, Flask CI/CD! web app made cheap and fast with Flask!!!!! I made this using jenkins."
